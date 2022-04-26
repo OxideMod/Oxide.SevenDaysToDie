cc406fe6063bb9f85a34eb0dc59402994993f62d
Luke Spragg <luke@spragg.me>
2022-04-25 20:40:48 -0400

    Expose various methods/fields for plugins
    Also include patched LogLibrary for plugins

--- src/SevenDaysToDieExtension.cs
+++ src/SevenDaysToDieExtension.cs
@@ -1,4 +1,4 @@
-using Oxide.Core;
+﻿using Oxide.Core;
 using Oxide.Core.Extensions;
 using Oxide.Plugins;
 using System;
@@ -46,7 +46,7 @@ namespace Oxide.Game.SevenDays
         /// </summary>
         public override string[] DefaultReferences => new[]
         {
-            "UnityEngine.AIModule", "UnityEngine.AssetBundleModule", "UnityEngine.CoreModule", "UnityEngine.GridModule", "UnityEngine.ImageConversionModule",
+            "LogLibrary", "UnityEngine.AIModule", "UnityEngine.AssetBundleModule", "UnityEngine.CoreModule", "UnityEngine.GridModule", "UnityEngine.ImageConversionModule",
             "UnityEngine.Networking", "UnityEngine.PhysicsModule", "UnityEngine.TerrainModule", "UnityEngine.TerrainPhysicsModule", "UnityEngine.UI", "UnityEngine.UIModule",
             "UnityEngine.UIElementsModule", "UnityEngine.UnityWebRequestAudioModule", "UnityEngine.UnityWebRequestModule", "UnityEngine.UnityWebRequestTextureModule",
             "UnityEngine.UnityWebRequestWWWModule", "UnityEngine.VehiclesModule", "UnityEngine.WebModule"
@@ -57,7 +57,7 @@ namespace Oxide.Game.SevenDays
         /// </summary>
         public override string[] WhitelistAssemblies => new[]
         {
-            "Assembly-CSharp", "mscorlib", "Oxide.Core", "System", "System.Core", "UnityEngine"
+            "Assembly-CSharp", "LogLibrary", "mscorlib", "Oxide.Core", "System", "System.Core", "UnityEngine"
         };
 
         /// <summary>
